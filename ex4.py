5d5fd3cd577256f22632e30977f0d03a13a579d2
Saglara <nohaevass@yandex.ru>
2023-07-22 11:35:28 +0300

    1

--- ex4.py
+++ ex4.py
@@ -1,7 +1,21 @@
 # ✔ Функция получает на вход список чисел.
 # ✔ Отсортируйте его элементы in place без использования встроенных в язык сортировок.
 # ✔ Как вариант напишите сортировку пузырьком. Её описание есть в википедии.
+from random import randint
+
+str_size = 10
+
+
+def buble_sort(sorted_list):
+    for i in range(str_size):
+        for j in range(str_size-i-1):
+            if sorted_list[j] > sorted_list[j+1]:
+                sorted_list[j], sorted_list[j+1] = sorted_list[j+1], sorted_list[j]
+    return sorted_list
 
 
 
+my_list = [randint(0, 100) for i in range(str_size)]
+buble_sort(my_list)
+print(my_list)
 
