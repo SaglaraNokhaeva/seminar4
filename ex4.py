36dd455791ae834a3a675a5b1b0dd77cf1a4dee5
Saglara <nohaevass@yandex.ru>
2023-07-22 11:44:35 +0300

    1

--- ex4.py
+++ ex4.py
@@ -8,12 +8,13 @@ str_size = 10
 
 def buble_sort(sorted_list):
     for i in range(str_size):
-        for j in range(str_size-i-1):
-            if sorted_list[j] > sorted_list[j+1]:
-                sorted_list[j], sorted_list[j+1] = sorted_list[j+1], sorted_list[j] #меняем j-й и (j+1)-й элементы местами
+        for j in range(str_size - i - 1):
+            if sorted_list[j] > sorted_list[j + 1]:
+                sorted_list[j], sorted_list[j + 1] = sorted_list[j + 1], sorted_list[
+                    j]  # меняем j-й и (j+1)-й элементы местами
 
 
-my_list = [randint(0, 100) for i in range(str_size)] # рандомно создаём лист из  str_size элементов в пределах от 0 до 99
+print(my_list := [randint(0, 100) for i in
+                  range(str_size)])  # рандомно создаём лист из  str_size элементов в пределах от 0 до 99
 buble_sort(my_list)
 print(my_list)
-
