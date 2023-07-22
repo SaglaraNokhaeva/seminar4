427dcb737bbbcd8fd076bf06da45b3b28b33503e
Saglara <nohaevass@yandex.ru>
2023-07-22 11:37:23 +0300

    1

--- ex4.py
+++ ex4.py
@@ -10,7 +10,7 @@ def buble_sort(sorted_list):
     for i in range(str_size):
         for j in range(str_size-i-1):
             if sorted_list[j] > sorted_list[j+1]:
-                sorted_list[j], sorted_list[j+1] = sorted_list[j+1], sorted_list[j]
+                sorted_list[j], sorted_list[j+1] = sorted_list[j+1], sorted_list[j] #меняем j-й и (j+1)-й элементы местами
 
 
 my_list = [randint(0, 100) for i in range(str_size)]
